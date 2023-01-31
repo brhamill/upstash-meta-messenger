a78de86f207a90fd12563bc3637bddeedda045c2
Brad Hamill <brad@hamill.com>
2023-01-30 22:29:39 -0700

    initial commit

---
+++ pusher.ts
@@ -0,0 +1,15 @@
+import Pusher from 'pusher'
+import ClientPusher from 'pusher-js'
+
+export const serverPusher = new Pusher({
+  appId: '1544791',
+  key: '1bfba5f136b6af54fed8',
+  secret: '73a6322579949cda2a36',
+  cluster: 'us3',
+  useTLS: true,
+})
+
+export const clientPusher = new ClientPusher('1bfba5f136b6af54fed8', {
+  cluster: 'us3',
+  forceTLS: true,
+})
