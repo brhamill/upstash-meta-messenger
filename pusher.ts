8c9909c2b7c75ef489efa2165dbe71fac254de40
Brad Hamill <brad@hamill.com>
2023-02-15 12:19:20 -0700

    update client pusher

--- pusher.ts
+++ pusher.ts
@@ -4,7 +4,7 @@ import ClientPusher from 'pusher-js'
 export const serverPusher = new Pusher({
   appId: '1544791',
   key: '1bfba5f136b6af54fed8',
-  secret: '73a6322579949cda2a36',
+  secret: '73a63<redacted>',
   cluster: 'us3',
   useTLS: true,
 })
